3a7e0285490122cb33474fd324fe7baa534882fa
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-09-17 13:49:47 +0300

    Update generateUniqueId.ts

--- src/utils/generateUniqueId.ts
+++ src/utils/generateUniqueId.ts
@@ -1,3 +1,9 @@
-export function generateUniqueId(): string {
+export function generateUniqueIdV1(): string {
   return Date.now() + '-' + Math.round(Math.random() * 1e9);
-}
+} // Example output: 1726138588544-138573341
+
+export function generateUniqueIdV2(): string {
+  const timestamp = Date.now().toString(36);
+  const randomPart = Math.random().toString(36).substring(2);
+  return `${timestamp}-${randomPart}`;
+} // Example output: lmg4efhc-1c3zk4f1
